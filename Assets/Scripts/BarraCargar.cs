cecb0d82c9a3c798ec5881b87a5ddc523949881a
Jesus Crsithian Yujra Huanca <kuollivo64@gmail.com>
2021-09-04 00:22:41 -0400

    Pantalla de Carga
    Se agrego lo que es un panel de carga antes de entrar a la cámara de realidad virtual.

---
+++ Assets/Scripts/BarraCargar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BarraCargar : MonoBehaviour
+{
+    public GameObject pantallaCarga;
+    public GameObject logoUnivalle;
+
+    public void CargarNivel(int numScene){
+        StartCoroutine(cargar(numScene));
+    }
+    IEnumerator cargar(int numScene){
+        AsyncOperation operacion = SceneManager.LoadSceneAsync(numScene);
+        pantallaCarga.SetActive(true);
+        logoUnivalle.SetActive(false);
+        while (!operacion.isDone)
+        {
+            yield return null;
+        }
+    }
+}
+
+
+
+
--- Assets/Scripts/InteraccionInfo.cs
+++ Assets/Scripts/InteraccionInfo.cs
@@ -1,15 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InteraccionInfo : MonoBehaviour
 {
     public string URL;
-
     private void OnMouseDown()
     {
         Application.OpenURL(URL);
     }
-
-
 }
