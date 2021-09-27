4737d0dce29439a248265fad36380f690006b553
Alvaro Gonzales <alvarojuan2014@gmail.com>
2021-09-27 18:38:46 -0400

    Mensaje de instruccion
    Mensaje que se mostrará al usuario con una breve instruccion de apuntar la camara hacia el codigo QR a una distancia de 1 metro aproximadamente

---
+++ Assets/Scripts/MensajeSinQR.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MensajeSinQR : MonoBehaviour
+{
+    [SerializeField]
+    GameObject panel;
+
+    public void onTargetActive()
+    {
+        panel.SetActive(false);
+    }
+
+    public void onTargetNoActive()
+    {
+        panel.SetActive(true);
+    }
+}
